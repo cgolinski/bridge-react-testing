12158cf97c0a7342c4aab8c05573c97c73c9bf5e
cgolinski <cgolinski@users.noreply.github.com>
2017-07-03 12:59:30 -0400

    fixed tests

--- src/components/cats-list.test.js
+++ src/components/cats-list.test.js
@@ -1,30 +1,25 @@
-//Make sure if give it array, get right amount of cat components
-//Write any other tests you think cats-list should have
-
 import React from 'react';
 import { shallow } from 'enzyme';
 import Cat from './cat';
 import CatsList from './cats-list';
 
-
 describe('cat component tests', () => {
   const props = {
     name: 'Snuggles',
-    picture: 'https://picsofsnugglez.com/1', 
+    picture: 'https://picsofsnugglez.com/1',
     cats: [{name: 'Snuggles', picture: 'https://picsofsnugglez.com/1'}, {name: 'Bob', picture: 'https://picsofsnugglez.com/2'}]
   };
 
-  /*it('should render cat\'s name', () => {
-    const wrapper = shallow(<Cat {...props}/>);
-    const name = 'Snuggles';
-    
-    expect(wrapper.contains(name)).toEqual(true);
-  });*/
+  it('should render cat component(s)', () => {
+    const wrapper = shallow(<CatsList {...props}/>);
 
-  it('should render correct number of cat components', () => {
+    expect(wrapper.find(Cat)).not.toHaveLength(0);
+  });
+
+  it('should render same number of cat components as props.cats', () => {
     const wrapper = shallow(<CatsList {...props}/>);
     const cats = [{name: 'Snuggles', picture: 'https://picsofsnugglez.com/1'}, {name: 'Bob', picture: 'https://picsofsnugglez.com/2'}];
-    
-    expect(wrapper.find(Cat)).toHaveLength(props.cats.length);
+
+    expect(wrapper.find(Cat)).toHaveLength(cats.length);
   });
 });
